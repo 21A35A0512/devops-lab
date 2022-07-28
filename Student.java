9b4ecf9d08c8f0ced08dbc8103565dd926f2fe25
20A31A0557 <20a31a0557@pragati.ac.in>
2022-07-28 15:21:08 +0530

    first.java

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student {    
+public static void main(String[] args) {    
+int x = 10;    
+int y = 12;  
+int sum=x+y;
+if(sum > 20) {    
+System.out.println("x + y sum is  "+sum);    
+}    
+}      
+}     
